aa8998d8480fda205cfa735a0e1b0294a96e6450
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2022-12-20 00:08:24 +0100

    Create example Program.cs

--- Exploration.SwitchExpressions/Program.cs
+++ Exploration.SwitchExpressions/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using Exploration.SwitchExpressions;
+
+var command = new Command((int?)null);
+
+var switchResult = command switch
+{
+    { IsString: true } when string.IsNullOrWhiteSpace(command.Text)  => "I'm an empty string",
+    { IsString: true }  => $"I'm a string saying: {command.Text}",
+    { IsNullableInt: true, Number: null } => "I'm a number, but null",
+    { IsNullableInt: true, Number: >= 0 } => "I'm a non-negative number",
+    { IsNullableInt: true } => "I'm just a number",
+    _ => throw new StrongTypingException($"Invalid coproduct of {nameof(command)}")
+};
+
+var matchResult = command.Match(
+    _ => "I'm a string",
+    _ => "I'm a number"
+);
+
+Console.WriteLine(switchResult);
+Console.WriteLine($"{matchResult} and that's all I can do :(");
